e7d8f9eae4a2ba5a66bf5ef29080e0314dc6e218
Thiago Felipe <thiagofelipe1002@gmail.com>
2022-12-02 22:58:41 -0300

    Implementando Automacao

--- src/test/java/page_objects/CaixaVerdePage.java
+++ src/test/java/page_objects/CaixaVerdePage.java
@@ -1,16 +1,22 @@
 package page_objects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class CaixaVerdePage {
     public ChromeDriver navegador;
-    public String url = "https://codepen.io/choskim/pen/RLYebL";
-    public String mapGreenBox;
+    public Actions actions;
+    private String url = "https://codepen.io/choskim/pen/RLYebL";
+    private By mapGreenBox = By.className("square");
 
     public void abrirPagina(){
         System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\webdrivers\\chromedriver.exe");
         navegador = new ChromeDriver();
+        actions = new Actions(navegador);
         navegador.get(url);
+        navegador.switchTo().frame(0);
     }
 
     public void fecharPagina(){
@@ -18,8 +24,11 @@ public class CaixaVerdePage {
         navegador = null;
     }
 
-    public void longclick() {
-
+    public void longclick() throws InterruptedException {
+        WebElement element = navegador.findElement(mapGreenBox);
+        actions.clickAndHold(element).build().perform();
+        Thread.sleep(1500);
+        actions.moveToElement(element).release();
     }
 
     public int[] getGreenBox(){
--- src/test/java/steps_definition/PressGestureSteps.java
+++ src/test/java/steps_definition/PressGestureSteps.java
@@ -1,20 +1,25 @@
 package steps_definition;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
+import page_objects.CaixaVerdePage;
 
 public class PressGestureSteps {
+    CaixaVerdePage pageCaixaVerde = new CaixaVerdePage();
+
     @Dado("^que o usuario esteja na pagina hammer\\.js$")
-    public void que_o_usuario_esteja_na_pagina_hammer_js() throws Throwable {
+    public void que_o_usuario_esteja_na_pagina_hammer_js() {
+        pageCaixaVerde.abrirPagina();
     }
 
     @Quando("^o usuario efetuar um longclick na caixa verde$")
-    public void o_usuario_efetuar_um_longclick_na_caixa_verde() throws Throwable {
+    public void o_usuario_efetuar_um_longclick_na_caixa_verde() throws InterruptedException {
+        pageCaixaVerde.longclick();
     }
 
     @Entao("^a caixa verde expande de tamanho para (\\d+) x (\\d+)$")
-    public void a_caixa_verde_expande_de_tamanho_para_x(int arg1, int arg2) throws Throwable {
+    public void a_caixa_verde_expande_de_tamanho_para_x(int x, int y)  {
+        pageCaixaVerde.fecharPagina();
     }
 }
