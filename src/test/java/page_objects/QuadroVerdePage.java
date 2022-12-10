ba41d946dce85e30e201e8e5437ce847caae6c33
Thiago Felipe <thiagofelipe1002@gmail.com>
2022-12-10 10:35:15 -0300

    Inserindo variáveis TimeLongClick e WebBrowserFrame

--- src/test/java/page_objects/QuadroVerdePage.java
+++ src/test/java/page_objects/QuadroVerdePage.java
@@ -9,23 +9,23 @@ import org.openqa.selenium.interactions.Actions;
 
 public class QuadroVerdePage {
 
-//  public ChromeDriver navegador;
     public FirefoxDriver navegador;
     public Actions actions;
     private String url = "https://codepen.io/choskim/pen/RLYebL";
     private By mapQuadroVerde = By.className("square");
+    public int TimeLongClick = 1500;
+    public int WebBrowserFrame = 0;
 
-    public void abrirPagina(){
-//        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\webdrivers\\chromedriver.exe");
-//        navegador = new ChromeDriver();
-        System.setProperty("webdriver.gecko.driver",  "src\\test\\resources\\webdrivers\\geckodriver.exe");
+
+    public void abrirPagina() {
+        System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\webdrivers\\geckodriver.exe");
         navegador = new FirefoxDriver();
         actions = new Actions(navegador);
         navegador.get(url);
-        navegador.switchTo().frame(0); //"sub-pagina"
+        navegador.switchTo().frame(WebBrowserFrame);
     }
 
-    public void fecharPagina(){
+    public void fecharPagina() {
         navegador.quit();
         navegador = null;
     }
@@ -33,11 +33,11 @@ public class QuadroVerdePage {
     public void longclick() throws InterruptedException {
         WebElement element = navegador.findElement(mapQuadroVerde);
         actions.clickAndHold(element).build().perform();
-        Thread.sleep(1500);
+        Thread.sleep(TimeLongClick);
         actions.moveToElement(element).release();
     }
 
-    public int[] getSizeQuadroVerde(){
+    public int[] getSizeQuadroVerde() {
         Dimension tamanho = navegador.findElement(mapQuadroVerde).getSize();
         return new int[]{tamanho.height, tamanho.width};
     }
